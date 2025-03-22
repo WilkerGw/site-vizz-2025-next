4f2cac7640968fa7c048ff7512d701c4a4f86a2d
Wilker Martins <dev.wilker@gmail.com>
2025-03-21 21:09:24 -0300

    nova secao de servicos

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,30 +1,34 @@
-'use client'
+"use client";
 
 import React from "react";
 import { motion } from "framer-motion";
 import styles from "../styles/components/features.module.css";
-import ServicesFilter from "../components/Services-filter";
 
 const Features = () => {
   return (
     <section id="servicos" className={styles.features}>
-      <h1>Nossos serviços</h1>
       <div className={styles.services}>
         {[
           {
             title: "Exame de Vista",
             highlight: "Gratuito",
-            description: "Agende seu exame de vista gratuito com profissionais qualificados. Cuidamos da sua saúde ocular com precisão e sem custo."
+            description:
+              "Agende seu exame de vista gratuito com profissionais qualificados. Cuidamos da sua saúde ocular com precisão e sem custo.",
+            image: "/images/mulher-sentada.png",
           },
           {
             title: "Fabricação de",
             highlight: "Óculos de Grau Personalizados",
-            description: "Criamos óculos sob medida para você! Escolha a armação perfeita e conte com lentes adaptadas às suas necessidades."
+            description:
+              "Criamos óculos sob medida para você! Escolha a armação perfeita e conte com lentes adaptadas às suas necessidades.",
+            image: "/images/homem-sentado.png",
           },
           {
             title: "Consultoria com",
             highlight: "Especialistas",
-            description: "Nossa equipe está pronta para te ajudar a escolher o melhor óculos para o seu estilo e conforto, garantindo qualidade e bem-estar."
+            description:
+              "Nossa equipe está pronta para te ajudar a escolher o melhor óculos para o seu estilo e conforto, garantindo qualidade e bem-estar.",
+            image: "/images/mulher2-sentada.png",
           },
           {
             title: "Trabalhamos com as",
@@ -34,8 +38,8 @@ const Features = () => {
               "/images/parceiros/hoya.png",
               "/images/parceiros/varilux-logo-0.png",
               "/images/parceiros/zeiss.png",
-            ]
-          }
+            ],
+          },
         ].map((service, index) => (
           <motion.div
             key={index}
@@ -45,11 +49,14 @@ const Features = () => {
             transition={{ duration: 0.8, delay: index * 0.2 }}
             viewport={{ once: true }}
           >
-            <ServicesFilter />
-            <h1>
-              {service.title} <span className={styles.span}>{service.highlight}</span>
-            </h1>
-            <p>{service.description}</p>
+            <div className={styles.serviceInfo}>
+              <h1>
+                {service.title}{" "}
+                <span className={styles.span}>{service.highlight}</span>
+              </h1>
+              <p>{service.description}</p>
+            </div>
+            <img src={service.image} alt="" className={styles.imageService} />
             {service.images && (
               <div className={styles.imageGallery}>
                 {service.images.map((image, imgIndex) => (
