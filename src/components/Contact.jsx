a213749fe535e934731dabf06d3b7eb5918d6125
Wilker Martins <dev.wilker@gmail.com>
2025-03-24 18:03:29 -0300

    detalhes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -96,7 +96,8 @@ const Contact = () => {
         </h1>
         <p className={styles.mapsSubtitle}>Av. Do Oratório, 4869, Vila Industrial, SP.</p>
         <div className={styles.mapContainer}>
-          <iframe
+          <iframe 
+            className={styles.mapIframe}
             src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d116993.37814058634!2d-46.6101910862598!3d-23.6027883597688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x94ce5d0dedc7ec5d%3A0xb59f5577f9728b75!2sAv.%20do%20Orat%C3%B3rio%2C%204869%20-%20Jardim%20Guairaca%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003221-200!3m2!1d-23.6028101!2d-46.5277895!5e0!3m2!1spt-BR!2sbr!4v1742826408619!5m2!1spt-BR!2sbr"
             width="100%"
             height="100%"
