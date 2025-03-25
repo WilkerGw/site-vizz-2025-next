06a471a3f0494ab96072d84eaacb4a33503e7389
Wilker Martins <dev.wilker@gmail.com>
2025-03-24 22:04:35 -0300

    commit

--- src/components/Faixa.jsx
+++ src/components/Faixa.jsx
@@ -3,40 +3,42 @@ import styles from '../styles/components/faixa.module.css';
 export default function Faixa () {
     return (
       <div className={styles.marqueeContainer}>
-        <div className={styles.marqueeContent}>
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-          <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
-          <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
-        </div>
+        <a href="https://www.oticasvizz.com.br/#agendamento" className={styles.linkFaixa}>
+          <div className={styles.marqueeContent}>
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+            <p>Agende seu exame <span className={styles.spanFaixa}>gratuitamente !</span></p>
+            <img src="./images/logo-cinza.png" className={styles.logoFaixa} alt="" />
+          </div>
+        </a>
       </div>
     );
   }
