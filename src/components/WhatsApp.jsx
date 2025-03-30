27894e084b63b415d36cdfa3898c99c322fd7c81
Wilker Martins <dev.wilker@gmail.com>
2025-03-30 10:14:10 -0300

    numero do whatsapp

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/whatsapp.module.css";
 
 const WhatsApp = () => {
   return <button className={styles.whatsappButton}>
-    <img src="/images/whatsapp.png" alt="Logo do WhatsApp" className={styles.whatsappLogo} />
+    <a href="https://wa.me/551123628799" target="_blank"><img src="/images/whatsapp.png" alt="Logo do WhatsApp" className={styles.whatsappLogo} /></a>
   </button>;
 };
 
